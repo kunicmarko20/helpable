5d9c5143cdb9e48ba88dd85fa5427781dd1cb943
kunicmarko20 <kunicmarko20@gmail.com>
2019-09-29 18:38:13 +0100

    Allow sha command to accept branch

--- helpable/src/command/newest_commit_sha.rs
+++ helpable/src/command/newest_commit_sha.rs
@@ -2,7 +2,10 @@ use github_client::github::GithubClient;
 
 #[derive(Debug, Default, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
-pub struct NewestCommitSha {}
+pub struct NewestCommitSha {
+    /// Branch to fetch the latest sha from
+    pub branch: Option<String>,
+}
 
 impl NewestCommitSha {
     pub fn execute(
@@ -10,7 +13,11 @@ impl NewestCommitSha {
         github_client: GithubClient,
         repository_name: &str,
     ) -> Result<(), String> {
-        let response = github_client.branch_info(repository_name, "master");
+
+        let response = github_client.branch_info(
+            repository_name,
+            &self.branch.clone().unwrap_or("master".to_string())
+        );
 
         println!("{}", response.sha());
 
